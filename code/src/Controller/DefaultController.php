1ef17f5bf0369a7f8523a2c542b9ca7caeb5e966
Dmitriy Fishman <fishman.code@gmail.com>
2020-11-10 14:28:01 +0200

    Created a default controller

---
+++ code/src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        return new JsonResponse('ok');
+    }
+}
